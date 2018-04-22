5150dfedffa23f6abb6710fbefa74c2e014b1ffa
Zaki Manian <zaki@manian.org>
2018-04-22 10:27:25 -0700

    Add readme and the extern crate directive

---
+++ README.md
@@ -0,0 +1,9 @@
+#Rust Amino
+
+Rust implementation of the Amino encoding standard.
+
+Tracking the reference golang implementation.
+
+Currently focusing on getting the basic encoding and decoding pieces together.
+
+Eventually will support amino-derive for macro based generation of encoding and decoding
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,25 @@
 #[macro_use]
-extern crate amino_derive;
 extern crate bytes;
+extern crate chrono;
 
 mod encoding;
 mod error;
 
 pub use error::{DecodeError, EncodeError};
+pub use encoding::*;
+// #[cfg(test)]
+// mod tests {
+//     #[derive(Wire)]
+//     struct Foo(i32);
 
-#[cfg(test)]
-mod tests {
-    #[derive(Wire)]
-    struct Foo(i32);
-
-    #[derive(Wire)]
-    struct Bar{
-        baz: i32
-    }
+//     #[derive(Wire)]
+//     struct Bar{
+//         baz: i32
+//     }
     
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
 
