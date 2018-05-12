30305d88f0e38d5df89a259ae054f43dbe34c880
Zaki Manian <zaki@manian.org>
2018-05-11 20:50:00 -0400

    Add check_field api

--- src/encoding.rs
+++ src/encoding.rs
@@ -70,6 +70,18 @@ pub fn decode_field_number_typ3<B>( buf: &mut B) ->Result<(u32,Typ3Byte),DecodeE
     return Ok((field_number as u32, typ3))
 }
 
+pub fn check_field_number_typ3<B>(buf: &mut B, expected_field_num: u32, expected_typ3: Typ3Byte)->Result<(), DecodeError>where B:Buf{
+
+    let (field_number , typ3) = decode_field_number_typ3(buf)?;
+    if field_number != expected_field_num{
+        return Err(DecodeError::new(format!("invalid field number for field {}",field_number)));   
+    }
+    if typ3 != expected_typ3 {
+                return Err(DecodeError::new(format!("invalid typ3 for field {}",field_number)));
+            }
+    return Ok(());
+}
+
 pub fn compute_disfix(identity: &str)->(Vec<u8>, Vec<u8>) {
     let mut sh = Sha256::default();
     sh.input(identity.as_bytes());
